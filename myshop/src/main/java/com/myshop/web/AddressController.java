1d3b08e07896aec276fea41f919d64a955dbe90a
Punit Dwivedi <punit.dwivedi.ext@bayer.com>
2022-08-31 13:14:08 -0600

    Changed the log size

--- myshop/src/main/java/com/myshop/web/AddressController.java
+++ myshop/src/main/java/com/myshop/web/AddressController.java
@@ -4,6 +4,9 @@ import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Address Controller
+ */
 @RequestMapping("/addresses")
 @Controller
 @RooWebScaffold(path = "addresses", formBackingObject = Address.class)
